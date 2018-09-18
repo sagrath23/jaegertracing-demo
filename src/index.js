3c829c06f7c747a90d2b825ff729fa6b32b4a780
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2018-09-18 14:37:27 -0500

    creating server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
-import { log } from './instruments/log'
+import http from 'http'
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+import initializeDb from './db'
+import middleware from './middleware'
+import api from './api'
+import config from './config.json'
 
-const formatString = (stringToFormat) => (`Hello, ${stringToFormat}!!!`)
-export const format = log()(formatString)
+let app = express()
+app.server = http.createServer(app)
 
-const helloTo = process.argv[2]
-console.log(format(helloTo))
+// logger
+app.use(morgan('dev'))
+
+// 3rd party middleware
+app.use(cors({
+	exposedHeaders: config.corsHeaders
+}))
+
+app.use(bodyParser.json({
+	limit : config.bodyLimit
+}))
+
+// app's router
+app.use('/api', api({ config}))
+
+app.server.listen(process.env.PORT || config.port, () => {
+    console.log(`Started on port ${app.server.address().port}`)
+});
+
+export default app
